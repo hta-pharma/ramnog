aad0400f2eded5d772b8f5fe6972da2881c8c8a6
Henrik Sparre Spiegelhauer (HSPU) <hspu@novonordisk.com>
2024-03-20 14:15:02 +0100

    Adds dummy test to allow workflows

--- R/dummy_fn.R
+++ R/dummy_fn.R
@@ -4,6 +4,9 @@
 #' @export
 #'
 #' @import data.table
-dummy_fn <- function(){
+dummy_fn <- function() {
+    # this function exists only to trigger imports and code-coverage
 
+    a <- 1 + 1
+    return(NULL)
 }
---
+++ tests/testthat/test-dummy_fn.R
@@ -0,0 +1,4 @@
+# Dummy function to trigger code-coverage
+test_that("Dummy function returns NULL", {
+    expect_null(dummy_fn())
+})
